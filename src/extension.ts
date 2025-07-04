05fb4f038e3917ddb5c36a31dd543fad4f12992c
mlgarchery <martial.garchery@gmail.com>
2025-07-04 16:15:12 +0200

    fix: save current branch in workspace state before entering detached HEAD mode

--- src/extension.ts
+++ src/extension.ts
@@ -103,21 +103,21 @@ const resetBranch = async (
   context: vscode.ExtensionContext,
   showMessage: boolean = false
 ) => {
-  const branch = context.workspaceState.get("branch");
+  const currentBranch = context.workspaceState.get("currentBranch");
 
-  if (!branch) {
+  if (!currentBranch) {
     if (showMessage) {
       vscode.window.showInformationMessage("No branch being compared found.");
     }
     return;
   }
 
-  execSync(`git switch ${branch}`);
-  context.workspaceState.update("branch", undefined);
+  execSync(`git switch ${currentBranch}`);
+  context.workspaceState.update("currentBranch", undefined);
 
   if (showMessage) {
     vscode.window.showInformationMessage(
-      `Switched back to branch ${branch} and out of detached HEAD mode.`
+      `Switched back to branch ${currentBranch} and out of detached HEAD mode.`
     );
   }
 };
@@ -147,13 +147,16 @@ export function activate(context: vscode.ExtensionContext) {
         placeHolder: "Branch to compare with main (default is current branch)",
       });
 
+      // save current branch before enterind detached HEAD mode
       const currentBranch = findCurrentBranch();
+      context.workspaceState.update("currentBranch", currentBranch);
 
       const [branch, compareToBranch] = parseBranchNames(input, currentBranch);
       fetchLatestChangesFromRemoteBranches(branch, compareToBranch);
 
       const branchHeadCommit = getLastCommitOnBranch(branch);
       const commonAncestor = getCommonAncestorCommit(branch, compareToBranch);
+
       enterDetachedHeadMode(branch, branchHeadCommit);
       resetSoftToCommonCommitAncestor(commonAncestor);
 
@@ -163,7 +166,6 @@ export function activate(context: vscode.ExtensionContext) {
         );
         return;
       }
-      context.workspaceState.update("branch", branch);
 
       vscode.commands.executeCommand("workbench.view.scm");
       vscode.window
